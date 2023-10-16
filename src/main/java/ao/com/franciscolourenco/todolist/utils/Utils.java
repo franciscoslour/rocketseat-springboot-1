1ab7a05287ab295674dd3974f196c04fe882e5d9
francisco.lourenco <franciscolour2011@gmail.com>
2023-10-16 09:21:08 +0100

    refactor: Utils

--- src/main/java/ao/com/franciscolourenco/todolist/utils/Utils.java
+++ src/main/java/ao/com/franciscolourenco/todolist/utils/Utils.java
@@ -12,6 +12,7 @@ public class Utils {
     private Utils(){
 
     }
+
     public static void copyNonNullProperties(Object source, Object target) {
         BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
     }
